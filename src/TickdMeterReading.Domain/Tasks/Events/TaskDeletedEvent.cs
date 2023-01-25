d370d847cdf0414b036d3e66ba380fbb70167e94
Dogelix <yeatmanjake@gmail.com>
2023-01-25 17:47:51 +0000

    Initial Commit

---
+++ src/TickdMeterReading.Domain/Tasks/Events/TaskDeletedEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TickdMeterReading.Domain.Tasks.Events
+{
+    public class TaskDeletedEvent : TaskEvent
+    {
+        public TaskDeletedEvent(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ tests/TickdMeterReading.Tests/UnitTests/Helpers/TaskViewModelHelper.cs
@@ -0,0 +1,20 @@
+﻿using TickdMeterReading.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TickdMeterReading.Tests.UnitTests.Helpers
+{
+    public static class TaskViewModelHelper
+    {
+        public static TaskViewModel GetTaskViewModel()
+        {
+            return new TaskViewModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                Summary = "Summary",
+                Description = "Description"
+            };
+        }
+    }
+}
